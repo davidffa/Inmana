12d09add38821c1733ca8de9ebda81dcb70d9796
David Amorim <davidfilipeamorim@gmail.com>
2021-08-13 20:06:09 +0100

    Implement scheduler with GenServer

---
+++ lib/inmana/supplies/scheduler.ex
@@ -0,0 +1,31 @@
+defmodule Inmana.Supplies.Scheduler do
+  use GenServer
+
+  alias Inmana.Supplies.ExpirationNotification
+
+  # CLIENT
+  def start_link(_state) do
+    GenServer.start_link(__MODULE__, %{})
+  end
+
+  # SERVER
+
+  @impl true
+  def init(state \\ %{}) do
+    schedule_notification()
+    {:ok, state}
+  end
+
+  @impl true
+  def handle_info(:generate, state) do
+    ExpirationNotification.send()
+
+    schedule_notification()
+
+    {:noreply, state}
+  end
+
+  defp schedule_notification do
+    Process.send_after(self(), :generate, 1000 * 10)
+  end
+end
